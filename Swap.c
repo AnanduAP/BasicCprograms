522adb657346e8ab2f4d698f40ef855c48777d5e
Anandu <anandhuap700@gmail.com>
2020-06-07 20:50:01 +0530

    Swap added other method include in comment part

--- Swap.c
+++ Swap.c
@@ -3,13 +3,13 @@
 int main(void)
 {
     int a,b,temp;
-    printf("Enter A");
+    printf("Enter A"); 
     scanf("%d",&a);
     printf("Enter B");
     scanf("%d",&b);
-    temp=a;
-    a=b;
-    b=temp;
+    temp=a;   /* else a=a+b; */
+    a=b;      /* b=a-b; */
+    b=temp;   /* a=a-b; */
     printf("Swapped Result \n A : %d B :%d",a,b);
     return EXIT_SUCCESS;
 }
