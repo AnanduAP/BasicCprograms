800f3b66c1b337f07941b19f1761e0efa3fa6b75
Anandu <anandhuap700@gmail.com>
2020-06-07 20:28:13 +0530

    Read and Addition pgms added

---
+++ ReadNum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+    int a;
+    printf("Enter a number");
+    scanf("%d",&a);
+    printf("Enterd number is :%d",a);
+    return EXIT_SUCCESS;
+}
